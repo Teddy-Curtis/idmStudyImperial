0c04693666a654800fbed03229a8bcae86e55094
Teddy-Curtis <TeddyMLCurtis@gmail.com>
2022-11-30 14:44:50 +0000

    getXS gets a list of the cross-sections

--- MadGraph_files/distributions/plotGenVariables.py
+++ MadGraph_files/distributions/plotGenVariables.py
@@ -10,24 +10,25 @@ import os
 # variables for different particles. 
 
 # define the process and the number of BPs
-process = 'h2h2lPlM_l_e_mu'
+process = 'h2h2lPlM_lem'
 num_BPs = 21
-base_dir = '/vols/cms/emc21/FCC/MadGraph_files/distributions'
+base_dir = '/vols/cms/emc21/idmStudy/MadGraph_files/distributions'
 
 
 
-def H_PT_scalarSum(H_data, dir):
-    mean = H_data['Particle.PT'].mean()
-    std = H_data['Particle.PT'].std()
+def H_PT(H_data, dir): 
+    #mean = H_data['Particle.PT'].mean()
+    #std = H_data['Particle.PT'].std()
     nbins = 100 
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(H_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
-    plt.title(f'Scalar sum of H PTs')
+    #plt.hist(H_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(H_data['Particle.PT'], bins=nbins)
+    plt.title(f'H PTs')
     plt.xlabel(f'PT (GeV)')
     plt.ylabel(f'Counts')
-    plt.legend()
-    plt.savefig(f'{dir}/H_PT_scalarSum.jpeg', bbox_inches='tight')
+    #plt.legend()
+    plt.savefig(f'{dir}/H_PT.jpeg', bbox_inches='tight')
     plt.close()
 
 
@@ -35,15 +36,16 @@ def H_PT_vectorSum(H_data, dir):
     grouped = H_data.groupby(level=0).sum()
     grouped['PT_tot'] = np.sqrt(grouped['Particle.Px']**2 + grouped['Particle.Py']**2)
     nbins = 100
-    mean = grouped['PT_tot'].mean()
-    std = grouped['PT_tot'].std()
+    #mean = grouped['PT_tot'].mean()
+    #std = grouped['PT_tot'].std()
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(grouped['PT_tot'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(grouped['PT_tot'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(grouped['PT_tot'], bins=nbins)
     plt.title(f'Vector sum of H PTs')
     plt.xlabel(f'PT (GeV)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/H_PT_vectorSum.jpeg', bbox_inches='tight')
     plt.close()
 
@@ -76,31 +78,33 @@ def getAngle(df):
 def H_angleDiff(H_data, dir):
     g = H_data.groupby('entry')
     angles = g.apply(getAngle)
-    mean = np.mean(angles)
-    std = np.std(angles)
+    #mean = np.mean(angles)
+    #std = np.std(angles)
     nbins = 100
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(angles, bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(angles, bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(angles, bins=nbins)
     plt.title(f'Angle between Hs')
     plt.xlabel(f'Angle (radians)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/H_angleDiff.jpeg', bbox_inches='tight')
     plt.close()
 
 
 def A_PT(A_data, dir):
-    mean = A_data['Particle.PT'].mean()
-    std = A_data['Particle.PT'].std()
+    #mean = A_data['Particle.PT'].mean()
+    #std = A_data['Particle.PT'].std()
     nbins = 100
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(A_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(A_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(A_data['Particle.PT'], bins=nbins)
     plt.title(f'A PT')
     plt.xlabel(f'PT (GeV)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/A_PT.jpeg', bbox_inches='tight')
     plt.close()
 
@@ -110,62 +114,66 @@ def HA_PT_vectorSum(data, dir):
     # Can then find the abs of this to get the actual E_miss
     inert_data = inert_data.groupby(level=0).sum()
     inert_data['PT_tot'] = np.sqrt(inert_data['Particle.Px']**2 + inert_data['Particle.Py']**2)
-    mean = inert_data['PT_tot'].mean()
-    std = inert_data['PT_tot'].std()
+    #mean = inert_data['PT_tot'].mean()
+    #std = inert_data['PT_tot'].std()
     nbins = 100
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(inert_data['PT_tot'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(inert_data['PT_tot'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(inert_data['PT_tot'], bins=nbins)
     plt.title(f'Vector sum of H and A PT')
     plt.xlabel(f'PT (GeV)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/HA_PT_vectorSum.jpeg', bbox_inches='tight')
     plt.close()
 
 
 def lepton_PT(lepton_data, dir):
-    mean = lepton_data['Particle.PT'].mean()
-    std = lepton_data['Particle.PT'].std()
+    #mean = lepton_data['Particle.PT'].mean()
+    #std = lepton_data['Particle.PT'].std()
     nbins = 100
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(lepton_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(lepton_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(lepton_data['Particle.PT'], bins=nbins)
     plt.title(f'Lepton PT')
     plt.xlabel(f'PT (GeV)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/lepton_PT.jpeg', bbox_inches='tight')
     plt.close()
 
 def lepton_angleDiff(lepton_data, dir):
     g = lepton_data.groupby('entry')
     angles = g.apply(getAngle)
-    mean = np.mean(angles)
-    std = np.std(angles)
+    #mean = np.mean(angles)
+    #std = np.std(angles)
     nbins = 100
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(angles, bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(angles, bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(angles, bins=nbins)
     plt.title(f'Angle Between Leptons')
     plt.xlabel(f'Angle (radians)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/lepton_angleDiff.jpeg', bbox_inches='tight')
     plt.close()
 
 def jet_PT(jet_data, dir):
     # Plot of PT
     nbins = 100
-    mean = jet_data['Particle.PT'].mean()
-    std = jet_data['Particle.PT'].std()
+    #mean = jet_data['Particle.PT'].mean()
+    #std = jet_data['Particle.PT'].std()
     plt.figure(figsize=(8,6))
     plt.tight_layout()
-    plt.hist(jet_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    #plt.hist(jet_data['Particle.PT'], bins=nbins, label=f'$\mu={mean:0.2f}$, $\sigma={std:0.2f}$')
+    plt.hist(jet_data['Particle.PT'], bins=nbins)
     plt.title(f'Jet PT')
     plt.xlabel(f'PT (GeV)')
     plt.ylabel(f'Counts')
-    plt.legend()
+    #plt.legend()
     plt.savefig(f'{dir}/jet_PT.jpeg', bbox_inches='tight')
     plt.close()
 
@@ -185,12 +193,12 @@ for i in range(1, 22):
     file = uproot.open(data_dir)
     tree = file['LHEF']
     tree = tree['Particle']
-    print(tree.keys())
+    
     data = tree.arrays(['Particle.PID', 'Particle.Px','Particle.Py', 'Particle.Pz', 'Particle.PT', 'Particle.E', 'Particle.Eta'], library='pd')
 
     # Now plot everything
     H_data = data[(data['Particle.PID'] == 35)]
-    H_PT_scalarSum(H_data, plot_dir)
+    H_PT(H_data, plot_dir)
     H_PT_vectorSum(H_data, plot_dir)
     H_angleDiff(H_data, plot_dir)
 
