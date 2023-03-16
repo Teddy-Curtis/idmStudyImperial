ed3877c4c7148bafc73082931bad43e12c221810
Teddy-Curtis <TeddyMLCurtis@gmail.com>
2023-03-16 14:28:08 +0000

    Edited so can create all the gridpacks in one command

--- gridpack_batch/qsub_gridpacks.sh
+++ gridpack_batch/qsub_gridpacks.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #$-q hep.q -l h_rt=4:0:0 -cwd
+BPNUM=$1
 ulimit -s unlimited
 source ~/.bashrc
 cd /vols/cms/emc21/idmStudy/genproductions/bin/MadGraph5_aMCatNLO
 
-./gridpack_generation.sh h2h2lPlM_lem_BP1 cards/iDM/h2h2lPlM_lem/h2h2lPlM_lem_BP1
+./gridpack_generation.sh h2h2lPlM_lem_BP$BPNUM cards/iDM/h2h2lPlM_lem/h2h2lPlM_lem_BP$BPNUM
+
+# for BP in 8 10 12 13 14 18 19 20 21 24; do qsub qsub_gridpacks.sh $BP; done
 
