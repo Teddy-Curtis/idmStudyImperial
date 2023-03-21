60a2e7176f9c7afeb8cde7a896f60c1c5f7f8b49
Teddy-Curtis <TeddyMLCurtis@gmail.com>
2023-03-21 13:23:07 +0000

    Just changed the CMSSW and scram version

--- gridpack_batch/qsub_gridpacks.sh
+++ gridpack_batch/qsub_gridpacks.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #$-q hep.q -l h_rt=4:0:0 -e /vols/cms/emc21/idmStudy/gridpack_batch/logging/ -o /vols/cms/emc21/idmStudy/gridpack_batch/logging/
 BPNUM=$1
-CMSSW_version="CMSSW_10_6_27"
-scram_version="slc7_amd64_gcc700"
-
+#CMSSW_version="CMSSW_10_6_27"
+CMSSW_version="CMSSW_9_4_7"
+#scram_version="slc7_amd64_gcc700"
+scram_version="slc6_amd64_gcc630"
 
 ulimit -s unlimited
 source ~/.bashrc
 cd /vols/cms/emc21/idmStudy/genproductions/bin/MadGraph5_aMCatNLO
 
-./gridpack_generation.sh h2h2lPlM_lem_BP$BPNUM cards/iDM/h2h2lPlM_lem/h2h2lPlM_lem_BP$BPNUM local ALL $scram_version $CMSSW_version
+./gridpack_generation.sh h2h2lPlM_lem_BP${BPNUM} cards/iDM/h2h2lPlM_lem/h2h2lPlM_lem_BP${BPNUM} local ALL ${scram_version} ${CMSSW_version}
 
 # for BP in 8 10 12 13 14 18 19 20 21 24; do qsub qsub_gridpacks.sh $BP; done
 
