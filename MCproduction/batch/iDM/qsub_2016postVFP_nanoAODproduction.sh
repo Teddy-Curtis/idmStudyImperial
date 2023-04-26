fa6c09098fce32ea069795ece3f6e94fbcc7dd02
ecurtis <edward.curtis@cern.ch>
2023-04-26 10:21:00 +0100

    Added qsub files for all years

---
+++ MCproduction/batch/iDM/qsub_2016postVFP_nanoAODproduction.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$-q hep.q -l h_rt=2:0:0 -pe hep.pe 4 -e /vols/cms/emc21/idmStudy/MCproduction/logging/ -o /vols/cms/emc21/idmStudy/MCproduction/logging/
+BPNUM=$1
+run_name="h2h2lPlM_lem"
+CMSSW_version="CMSSW_10_6_19"
+scram_version="slc7_amd64_gcc700"
+nevents=100
+ncpus=4
+
+# Now get the arguments
+tag="${run_name}_BP${BPNUM}"
+gridpack="/vols/cms/emc21/idmStudy/myFiles/gridpacks/${run_name}_${CMSSW_version}/${tag}/${tag}_${scram_version}_${CMSSW_version}_tarball.tar.xz"
+fragment="/vols/cms/emc21/idmStudy/MCproduction/fragments/iDM/general_config.py"
+output_folder="/vols/cms/emc21/idmStudy/myFiles/gridpacks/${run_name}_${CMSSW_version}/${tag}"
+
+ulimit -s unlimited
+source ~/.bashrc
+cd /vols/cms/emc21/idmStudy/MCproduction
+
+
+/vols/cms/emc21/idmStudy/MCproduction/RunIISummer16_postVFP_nanoAOD.sh ${gridpack} ${fragment} ${nevents} ${ncpus} ${output_folder}
+# for BP in 8 10 12 13 14 18 19 20 21 24; do qsub qsub_2016postVFP_nanoAODproduction.sh $BP; done
+# for _ in {1..10}; do for BP in 8 10 12 13 14 18 19 20; do qsub qsub_2016postVFP_nanoAODproduction.sh $BP; done; done;
--- MCproduction/batch/iDM/qsub_h2h2lPlM_lem.sh
+++ MCproduction/batch/iDM/qsub_h2h2lPlM_lem.sh
@@ -2,19 +2,21 @@
 #$-q hep.q -l h_rt=8:0:0 -e /vols/cms/emc21/idmStudy/MCproduction/logging/ -o /vols/cms/emc21/idmStudy/MCproduction/logging/
 BPNUM=$1
 run_name="h2h2lPlM_lem"
-nevents=30000
+CMSSW_version="CMSSW_10_2_22"
+scram_version="slc6_amd64_gcc700"
+nevents=10
 ncpus=1
 
 # Now get the arguments
 tag="${run_name}_BP${BPNUM}"
-gridpack="/vols/cms/emc21/idmStudy/myFiles/gridpacks/${run_name}/${tag}/${tag}_slc7_amd64_gcc700_CMSSW_10_6_27_tarball.tar.xz"
+gridpack="/vols/cms/emc21/idmStudy/myFiles/gridpacks/${run_name}_${CMSSW_version}/${tag}/${tag}_${scram_version}_${CMSSW_version}_tarball.tar.xz"
 fragment="/vols/cms/emc21/idmStudy/MCproduction/fragments/iDM/general_config.py"
-output_folder="/vols/cms/emc21/idmStudy/myFiles/gridpacks/${run_name}/${tag}"
+output_folder="/vols/cms/emc21/idmStudy/myFiles/gridpacks/${run_name}_${CMSSW_version}/${tag}"
 
 ulimit -s unlimited
 source ~/.bashrc
 cd /vols/cms/emc21/idmStudy/MCproduction
 
 
-/vols/cms/emc21/idmStudy/MCproduction/RunIIFall17_AODSIMGEN.sh ${gridpack} ${fragment} ${nevents} ${ncpus} ${output_folder}
+/vols/cms/emc21/idmStudy/MCproduction/RunInSteps.sh ${gridpack} ${fragment} ${nevents} ${ncpus} ${output_folder} #${CMSSW_version} ${scram_version}
 # for BP in 8 10 12 13 14 18 19 20 21 24; do qsub qsub_h2h2lPlM_lem.sh $BP; done
