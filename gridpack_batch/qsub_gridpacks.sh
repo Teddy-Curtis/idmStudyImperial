57074193acc8333e9e5613ad9e547a55b885592d
ecurtis <edward.curtis@cern.ch>
2023-05-11 16:32:22 +0100

    Create h2h2lPlMnunu_lemt_nuemt gridpacks

--- gridpack_batch/qsub_gridpacks.sh
+++ gridpack_batch/qsub_gridpacks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #$-q hep.q -l h_rt=1:0:0 -e /vols/cms/emc21/idmStudy/gridpack_batch/logging/ -o /vols/cms/emc21/idmStudy/gridpack_batch/logging/
 BPNUM=$1
+
+# define the process name here
+PROCESS="h2h2lPlMnunu_lem_nuemt"
+echo "Making gridpack for process" $PROCESS "for BP" $BPNUM
 #CMSSW_version="CMSSW_10_6_27"
 #CMSSW_version="CMSSW_9_4_7"
 #CMSSW_version="CMSSW_10_2_22"
@@ -16,7 +20,8 @@ ulimit -s unlimited
 source ~/.bashrc
 cd /vols/cms/emc21/idmStudy/genproductions/bin/MadGraph5_aMCatNLO
 
-./gridpack_generation.sh h2h2lPlM_lem_BP${BPNUM} cards/iDM/h2h2lPlM_lem/h2h2lPlM_lem_BP${BPNUM} local ALL ${scram_version} ${CMSSW_version}
+./gridpack_generation.sh ${PROCESS}_BP${BPNUM} cards/iDM/${PROCESS}/${PROCESS}_BP${BPNUM} local ALL ${scram_version} ${CMSSW_version}
 
 # for BP in 8 10 12 13 14 18 19 20 21 24; do qsub qsub_gridpacks.sh $BP; done
-
+# for BP in 1 2 3 4 5 6 7 9 11 15 16 17; do qsub qsub_gridpacks.sh $BP; done
+# for BP in {1..20}; do qsub qsub_gridpacks.sh $BP; done
