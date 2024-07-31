2bba9e85291a8f1dd08deca7e79c65b049104269
Teddy-Curtis <TeddyMLCurtis@gmail.com>
2024-07-31 16:01:08 +0200

    Get for Run3 as well

--- MCproduction/PU_lists/getPUlist.py
+++ MCproduction/PU_lists/getPUlist.py
@@ -5,12 +5,20 @@
 import subprocess
 
 files = {
-    "2017" : "/Neutrino_E-10_gun/RunIISummer20ULPrePremix-UL17_106X_mc2017_realistic_v6-v3/PREMIX"
+    "2016" : "/Neutrino_E-10_gun/RunIISummer20ULPrePremix-UL16_106X_mcRun2_asymptotic_v13-v1/PREMIX",
+    "2017" : "/Neutrino_E-10_gun/RunIISummer20ULPrePremix-UL17_106X_mc2017_realistic_v6-v3/PREMIX",
+    "2018" : "/Neutrino_E-10_gun/RunIISummer20ULPrePremix-UL18_106X_upgrade2018_realistic_v11_L1v1-v2/PREMIX",
+    "2022" : "/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX",
+    "2022EE" : "/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX"
 }
 
 
 names = {
-    "2017" : "Fall17"
+    "2016" : "16",
+    "2017" : "Fall17",
+    "2018" : "Autumn18",
+    "2022" : "2022",
+    "2022EE" : "2022EE"
 }
 
 
